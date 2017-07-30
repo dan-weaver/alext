42191b33eed7e951c280a690e20b65c45aebea2e
Dan Weaver <danweaver06@gmail.com>
2017-07-30 17:15:08 -0500

    configure build

---
+++ circle_deploy.sh
@@ -0,0 +1,15 @@
+ssh $DROPLET_USER@DROPLET_IP 'docker stop alext'
+ssh $DROPLET_USER@DROPLET_IP 'docker rm alext'
+
+ssh $DROPLET_USER@DROPLET_IP 'docker rm alext'
+
+
+echo "pulling image"
+ssh $DROPLET_USER@DROPLET_IP 'docker pull danweaver06/alext:latest'
+
+
+echo "starting new container"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name alext -p 80:80 danweaver06/alext:latest'
+
+echo "Success"
+exit 0
