1121fd9ac96a5d1cb03142ce8088fc53dfb545de
Dan Weaver <danweaver06@gmail.com>
2017-07-30 19:08:10 -0500

    more deployment debugging

--- circle_deploy.sh
+++ circle_deploy.sh
@@ -1,15 +1,11 @@
+echo 'accessing app server'
+echo 'stopping container'
 ssh $DROPLET_USER@$DROPLET_IP 'docker stop alext'
+echo 'removing container'
 ssh $DROPLET_USER@$DROPLET_IP 'docker rm alext'
-
-ssh $DROPLET_USER@$DROPLET_IP 'docker rm alext'
-
-
 echo "pulling image"
 ssh $DROPLET_USER@$DROPLET_IP 'docker pull danweaver06/alext:latest'
-
-
 echo "starting new container"
 ssh $DROPLET_USER@$DROPLET_IP 'docker run -d --restart=always --name alext -p 80:80 danweaver06/alext:latest'
-
 echo "Success"
 exit 0
